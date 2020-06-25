460e126d370ebe7e3ebb6c50bca114cb0653c366
Marco Heyden <heyden@ipe-nilpferd.local>
2020-06-25 09:43:00 +0200

    Added parameter grid search

--- src/outlying_partitions/evaluation.py
+++ src/outlying_partitions/evaluation.py
@@ -21,8 +21,22 @@ def create_ensembles(shape, l_name, contamination=0.01):
     num_clients = shape[0]
     c = create_models(num_clients, shape[-1], compression_factor=0.4)
     l = None
-    if l_name == "lof":
-        l = [LocalOutlierFactor(n_neighbors=20, contamination=contamination, novelty=True) for _ in range(num_clients)]
+    if l_name == "lof1":
+        l = [LocalOutlierFactor(n_neighbors=1, contamination=contamination, novelty=True) for _ in range(num_clients)]
+    if l_name == "lof2":
+        l = [LocalOutlierFactor(n_neighbors=2, contamination=contamination, novelty=True) for _ in range(num_clients)]
+    if l_name == "lof4":
+        l = [LocalOutlierFactor(n_neighbors=4, contamination=contamination, novelty=True) for _ in range(num_clients)]
+    if l_name == "lof8":
+        l = [LocalOutlierFactor(n_neighbors=8, contamination=contamination, novelty=True) for _ in range(num_clients)]
+    if l_name == "lof16":
+        l = [LocalOutlierFactor(n_neighbors=16, contamination=contamination, novelty=True) for _ in range(num_clients)]
+    if l_name == "lof32":
+        l = [LocalOutlierFactor(n_neighbors=32, contamination=contamination, novelty=True) for _ in range(num_clients)]
+    if l_name == "lof64":
+        l = [LocalOutlierFactor(n_neighbors=64, contamination=contamination, novelty=True) for _ in range(num_clients)]
+    if l_name == "lof100":
+        l = [LocalOutlierFactor(n_neighbors=100, contamination=contamination, novelty=True) for _ in range(num_clients)]
     if l_name == "xstream":
         l = [Chains(k=50, nchains=10, depth=10) for _ in range(num_clients)]
     if l_name == "ae":
