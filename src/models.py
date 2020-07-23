58242aa5faa87087f6939a141b56750332800dee
Marco Heyden <heyden@ipe-nilpferd.local>
2020-07-23 14:34:16 +0200

    Changed conv. autoencoder

--- src/models.py
+++ src/models.py
@@ -32,20 +32,20 @@ def create_model(dims, compression_factor):
 def create_deep_model(dims=(28, 28, 1)):
     input_img = Input(shape=dims)
 
-    x = Conv2D(32, (3, 3), activation='relu', padding='same',
-               kernel_regularizer=tf.keras.regularizers.l2())(input_img)
+    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
     x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(32, (3, 3), activation='relu', padding='same',
-               kernel_regularizer=tf.keras.regularizers.l2())(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
     encoded = MaxPooling2D((2, 2), padding='same')(x)
 
-    # at this point the representation is (7, 7, 32)
+    # at this point the representation is (4, 4, 8) i.e. 128-dimensional
 
-    x = Conv2D(32, (3, 3), activation='relu', padding='same',
-               kernel_regularizer=tf.keras.regularizers.l2())(encoded)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
     x = UpSampling2D((2, 2))(x)
-    x = Conv2D(32, (3, 3), activation='relu', padding='same',
-               kernel_regularizer=tf.keras.regularizers.l2())(x)
+    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+    x = UpSampling2D((2, 2))(x)
+    x = Conv2D(16, (3, 3), activation='relu')(x)
     x = UpSampling2D((2, 2))(x)
     decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
 
