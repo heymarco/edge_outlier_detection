9845e5533e7700ef1be732abfcaa66b4d6887918
Marco Heyden <heyden@ipe-nilpferd.local>
2020-08-03 14:05:08 +0200

    Regenerate data at every rep

--- EXP_outlying_partitions.py
+++ EXP_outlying_partitions.py
@@ -15,15 +15,18 @@ def create_datasets(args):
     if args.vary == "frac":
         frac_range = [0.0, 0.01, 0.03, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4]
         for frac in frac_range:
-            os.system(os.path.join(os.getcwd(), "GEN_outlying_partitions.py -sf {}".format(frac)))
+            data_generator = os.path.join(os.getcwd(), "GEN_outlying_partitions.py -sf {}".format(frac))
+            os.system("{} {}".format("python", data_generator))
     if args.vary == "cont":
         frac_range = [0.0, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
         for frac in frac_range:
-            os.system(os.path.join(os.getcwd(), "GEN_outlying_partitions.py -cont {}".format(frac)))
+            data_generator = os.path.join(os.getcwd(), "GEN_outlying_partitions.py -cont {}".format(frac))
+            os.system("{} {}".format("python", data_generator))
     if args.vary == "shift":
         frac_range = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0, 1.2, 1.5, 2.0, 2.5, 3.0]
         for frac in frac_range:
-            os.system(os.path.join(os.getcwd(), "GEN_outlying_partitions.py -shift {}".format(frac)))
+            data_generator = os.path.join(os.getcwd(), "GEN_outlying_partitions.py -shift {}".format(frac))
+            os.system("{} {}".format("python", data_generator))
 
     # load, trim, normalize data
     data = {}
